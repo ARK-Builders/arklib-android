df3b64ae18f608afe175bf5b209b0d824de33d13
mdrlzy <mordeniusss@gmail.com>
2023-03-29 20:15:36 +0600

    preview indexing flow

--- lib/src/main/java/space/taran/arklib/domain/preview/AggregatedPreviewStorage.kt
+++ lib/src/main/java/space/taran/arklib/domain/preview/AggregatedPreviewStorage.kt
@@ -1,12 +1,30 @@
 package space.taran.arklib.domain.preview
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.zip
 import space.taran.arklib.ResourceId
+import space.taran.arklib.app
 import space.taran.arklib.domain.index.ResourceMeta
 import java.nio.file.Path
 
 class AggregatedPreviewStorage(
-    private val shards: Collection<PlainPreviewStorage>
+    private val shards: Collection<PlainPreviewStorage>,
+    private val appScope: CoroutineScope
 ) : PreviewStorage {
+
+    private val _indexingFlow = MutableStateFlow(false)
+
+    init {
+        initShardsIndexingListener()
+    }
+
+    override val indexingFlow = _indexingFlow.asStateFlow()
 
     override fun locate(path: Path, resource: ResourceMeta): PreviewAndThumbnail? {
         shards.forEach { shard ->
@@ -30,4 +48,14 @@ class AggregatedPreviewStorage(
             require(it != null) { "At least one of shards must yield success" }
             it.store(path, meta)
         }
+
+    private fun initShardsIndexingListener() {
+        fun anyShardIndexing() = shards.map { it.indexingFlow.value }.contains(true)
+
+        shards.forEach { shard ->
+            shard.indexingFlow.onEach {
+                _indexingFlow.emit(anyShardIndexing())
+            }.launchIn(appScope)
+        }
+    }
 }
--- lib/src/main/java/space/taran/arklib/domain/preview/PreviewStorage.kt
+++ lib/src/main/java/space/taran/arklib/domain/preview/PreviewStorage.kt
@@ -1,10 +1,13 @@
 package space.taran.arklib.domain.preview
 
+import kotlinx.coroutines.flow.StateFlow
 import space.taran.arklib.ResourceId
 import space.taran.arklib.domain.index.ResourceMeta
 import java.nio.file.Path
 
 interface PreviewStorage {
+
+    val indexingFlow: StateFlow<Boolean>
 
     fun locate(path: Path, resource: ResourceMeta): PreviewAndThumbnail?
 
