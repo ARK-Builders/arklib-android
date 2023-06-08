a80123dfd9c477e4ed97b8a94653badbd64e0e20
mdrlzy <mordeniusss@gmail.com>
2023-06-09 01:21:22 +0600

    TagStorage send stats

--- lib/src/main/java/space/taran/arklib/domain/tags/RootTagsStorage.kt
+++ lib/src/main/java/space/taran/arklib/domain/tags/RootTagsStorage.kt
@@ -1,23 +1,56 @@
 package space.taran.arklib.domain.tags
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import space.taran.arklib.ResourceId
 import space.taran.arklib.arkFolder
 import space.taran.arklib.arkTags
+import space.taran.arklib.domain.stats.StatsEvent
 import space.taran.arklib.domain.storage.FileStorage
 import java.nio.file.Path
 
 class RootTagsStorage(
-    scope: CoroutineScope,
-    val root: Path
+    private val scope: CoroutineScope,
+    val root: Path,
+    private val statsFlow: MutableSharedFlow<StatsEvent>,
 ) : FileStorage<Tags>(
     scope, root.arkFolder().arkTags(), TagsMonoid, "tags"
 ), TagStorage {
 
     override fun valueToString(value: Tags): String =
         value.joinToString(",")
+
     override fun valueFromString(raw: String): Tags =
         raw.split(',')
             .map { it.trim() }
             .filter { it.isNotEmpty() }
             .toSet()
+
+    override fun remove(id: ResourceId) {
+        scope.launch {
+            statsFlow.emit(
+                StatsEvent.TagsChanged(
+                    id,
+                    oldTags = getValue(id),
+                    newTags = emptySet()
+                )
+            )
+        }
+        super.remove(id)
+    }
+
+    override fun setTags(id: ResourceId, tags: Tags) {
+        scope.launch {
+            statsFlow.emit(
+                StatsEvent.TagsChanged(
+                    id,
+                    oldTags = getValue(id),
+                    newTags = tags
+                )
+            )
+        }
+        super.setTags(id, tags)
+    }
 }
