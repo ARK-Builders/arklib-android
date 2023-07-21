88013c726beec9f468a1c5618c960408bb08c223
Kirill Taran <kirill@taran.space>
2023-07-21 14:01:54 +0300

    PropertiesStorage with title and description

---
+++ lib/src/main/java/space/taran/arklib/user/properties/Properties.kt
@@ -0,0 +1,30 @@
+package space.taran.arklib.user.properties
+
+import android.util.Log
+import kotlinx.serialization.Serializable
+import space.taran.arklib.domain.storage.Monoid
+
+@Serializable
+class Properties(
+    val titles: Set<String>,
+    val descriptions: Set<String>
+)
+
+object PropertiesMonoid: Monoid<Properties> {
+    override val neutral: Properties = Properties(
+        emptySet(),
+        emptySet()
+    )
+
+    override fun combine(a: Properties, b: Properties): Properties {
+        val result = Properties(
+            a.titles.union(b.titles),
+            a.descriptions.union(b.descriptions),
+        )
+
+        Log.v(LOG_PREFIX, "merging $a and $b into $result")
+        return result
+    }
+}
+
+internal val LOG_PREFIX: String = "[properties]"
