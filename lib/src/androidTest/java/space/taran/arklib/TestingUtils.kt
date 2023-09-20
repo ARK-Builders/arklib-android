22c478990a2d562315f67074b6d7630d69f624b0
Tanay <tanay1400089@gmail.com>
2023-09-20 14:51:14 +0530

    android test removed

--- lib/src/androidTest/java/space/taran/arklib/TestingUtils.kt
+++
@@ -1,9 +0,0 @@
-package space.taran.arklib
-
-import dev.arkbuilders.arklib.ResourceId
-
-fun randomResourceId():ResourceId =
-    ResourceId(
-        (Math.random() * 1000).toLong() + 1,
-        (Math.random() * 1000).toLong(),
-        )
