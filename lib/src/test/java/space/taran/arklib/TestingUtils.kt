07249ceaf8c907cf3728ece5215f4d9c6367f359
Tanay <tanay1400089@gmail.com>
2023-09-30 00:08:18 +0530

    old tests renamed, multiple calls test added

--- lib/src/test/java/space/taran/arklib/TestingUtils.kt
+++ lib/src/test/java/space/taran/arklib/TestingUtils.kt
@@ -3,11 +3,23 @@ package space.taran.arklib
 import dev.arkbuilders.arklib.ResourceId
 import dev.arkbuilders.arklib.user.score.Score
 
-fun randomResourceId():ResourceId =
+fun randomResourceId(): ResourceId =
     ResourceId(
         (Math.random() * 1000).toLong() + 1,
         (Math.random() * 1000).toLong(),
-        )
+    )
 
-fun randomScore(max:Int = 1000):Score =
+fun randomScore(max: Int = 1000): Score =
     (Math.random() * (max - 1)).toInt() + 1
+
+fun randomPositiveInt(max: Int = 1000): Int {
+    if (max < 10) {
+        throw Exception("parameter max value must be greater than equal to 10")
+    }
+    val value = (Math.random() * max).toInt()
+    return if (value > 0) {
+        value
+    } else {
+        randomPositiveInt()
+    }
+}
--- lib/src/test/java/space/taran/arklib/user/score/RootScoreStorageTests.kt
+++ lib/src/test/java/space/taran/arklib/user/score/RootScoreStorageTests.kt
@@ -1,45 +1,42 @@
 package space.taran.arklib.user.score
 
+import dev.arkbuilders.arklib.ResourceId
 import dev.arkbuilders.arklib.user.score.RootScoreStorage
+import dev.arkbuilders.arklib.user.score.Score
 import dev.arkbuilders.arklib.user.score.ScoreMonoid
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import space.taran.arklib.randomPositiveInt
 import space.taran.arklib.randomResourceId
 import space.taran.arklib.randomScore
 import java.nio.file.Path
 
 class RootScoreStorageTests {
 
-    private val scope: CoroutineScope by lazy {
-        GlobalScope
-    }
-
     private val pathMock: Path by lazy {
         mockk()
     }
-
-    private lateinit var rootScoreStorage: RootScoreStorage
 
     @Before
     fun setup() {
         coEvery { pathMock.resolve(any<String>()) } returns pathMock
         coEvery { pathMock.resolve(any<Path>()) } returns pathMock
-        rootScoreStorage = RootScoreStorage(scope, pathMock)
     }
 
     @Test
-    fun verifyGetScoreIfNoData() {
+    fun getScoreReturnsScoreMonidIfNotData() {
+        val rootScoreStorage = RootScoreStorage(GlobalScope, pathMock)
         val resourceId = randomResourceId()
         assertEquals(rootScoreStorage.getScore(resourceId), ScoreMonoid.neutral)
     }
 
     @Test
-    fun verifySetScore() {
+    fun setScoreResultsInCorrectValueBeingReturned() {
+        val rootScoreStorage = RootScoreStorage(GlobalScope, pathMock)
         val resourceId = randomResourceId()
         val score = randomScore()
         rootScoreStorage.setScore(resourceId, score)
@@ -47,7 +44,8 @@ class RootScoreStorageTests {
     }
 
     @Test
-    fun verifyRemove() {
+    fun callToRemoveClearsCorrespondingData() {
+        val rootScoreStorage = RootScoreStorage(GlobalScope, pathMock)
         val resourceId = randomResourceId()
         val score = randomScore()
         rootScoreStorage.setScore(resourceId, score)
@@ -56,5 +54,69 @@ class RootScoreStorageTests {
         rootScoreStorage.remove(resourceId)
         result = rootScoreStorage.getScore(resourceId)
         assertEquals(result, ScoreMonoid.neutral)
+    }
+
+    @Test
+    fun multipleFunctionCallsInSerialOrder() {
+        val rootScoreStorage = RootScoreStorage(GlobalScope, pathMock)
+
+        val totalSetRandomValue = randomPositiveInt(10) + 20
+        val map = mutableMapOf<ResourceId, Score>()
+
+        for (index in 1..totalSetRandomValue) {
+            val resourceId = randomResourceId()
+            val score = randomScore()
+            map[resourceId] = score
+            rootScoreStorage.setScore(resourceId, score)
+        }
+
+        for (resourceId in map.keys) {
+            val collectedValue = rootScoreStorage.getValue(resourceId)
+            val expectedValue = map[resourceId]
+            assertEquals(expectedValue, collectedValue)
+        }
+
+        for (resourceId in map.keys) {
+            rootScoreStorage.remove(resourceId)
+            val collectedValue = rootScoreStorage.getValue(resourceId)
+            assertEquals(collectedValue, ScoreMonoid.neutral)
+        }
+    }
+
+    @Test
+    fun multipleFunctionCallsInRandomisedOrder() {
+        val rootScoreStorage = RootScoreStorage(GlobalScope, pathMock)
+
+        val maxNumberOfCalls = randomPositiveInt(20) + 20
+
+        val randomIntervals = mutableSetOf<Int>()
+        var index = 0
+        while (index < 5) {
+            val value = randomPositiveInt(maxNumberOfCalls)
+            if (randomIntervals.contains(value).not()) {
+                index++
+                randomIntervals.add(value)
+            }
+        }
+
+        val mapOfResIdToScore = mutableMapOf<ResourceId, Score>()
+
+        for (i in 1..maxNumberOfCalls) {
+            val randomResourceId = randomResourceId()
+            val randomScore = randomScore()
+            mapOfResIdToScore[randomResourceId] = randomScore
+            rootScoreStorage.setScore(randomResourceId, randomScore)
+
+            if (randomIntervals.contains(i)) {
+                for (resId in mapOfResIdToScore.keys) {
+                    val expectedValue = mapOfResIdToScore[resId]
+                    val collectedValue = rootScoreStorage.getValue(resId)
+                    assertEquals(expectedValue, collectedValue)
+                }
+                mapOfResIdToScore.remove(randomResourceId)
+                rootScoreStorage.remove(randomResourceId)
+                assertEquals(rootScoreStorage.getScore(randomResourceId), ScoreMonoid.neutral)
+            }
+        }
     }
 }
