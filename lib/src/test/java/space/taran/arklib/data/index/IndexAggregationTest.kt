615b6e0ffce4578a050acb64927b82dafa2c4d6c
oluiscabral <oluiscabral@hotmail.com>
2023-09-30 01:29:21 -0300

    Implement tests for IndexAggregation remaining methods

--- lib/src/test/java/space/taran/arklib/data/index/IndexAggregationTest.kt
+++ lib/src/test/java/space/taran/arklib/data/index/IndexAggregationTest.kt
@@ -5,28 +5,30 @@ import dev.arkbuilders.arklib.data.index.IndexAggregation
 import dev.arkbuilders.arklib.data.index.Resource
 import dev.arkbuilders.arklib.data.index.ResourceUpdates
 import dev.arkbuilders.arklib.data.index.RootIndex
+import io.mockk.*
 import io.mockk.InternalPlatformDsl.toStr
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.unmockkAll
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.runBlocking
 import org.junit.After
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import java.nio.file.Path
 import java.nio.file.attribute.FileTime
 import java.time.Instant
 import java.util.*
+import kotlin.io.path.Path
 import kotlin.random.Random
 
 class IndexAggregationTest {
 
+    private var updatedShards: Int = 0
     private lateinit var shards: Set<RootIndex>
     private lateinit var indexAggregation: IndexAggregation
 
     @Before
     fun beforeEach() {
+        updatedShards = 0
         shards = createShards(3)
         indexAggregation = IndexAggregation(shards)
     }
@@ -48,9 +50,23 @@ class IndexAggregationTest {
     private fun createRootIndex(): RootIndex {
         val rootIndex = mockk<RootIndex>()
         val resources = createRandomResources(3)
+        val resourceIdSlot = CapturingSlot<ResourceId>()
+        val resourcePaths = createResourceRandomPaths(resources)
+        every { rootIndex.allPaths() } returns resourcePaths
         every { rootIndex.allIds() } returns resources.map { it.id }.toSet()
         every { rootIndex.updates } returns MutableSharedFlow<ResourceUpdates>()
         every { rootIndex.allResources() } returns resources.associateBy({ it.id }, { it })
+        every { rootIndex.getResource(capture(resourceIdSlot)) } answers {
+            val resourceId = resourceIdSlot.captured
+            resources.firstOrNull { resource -> resource.id == resourceId }
+        }
+        every { rootIndex.getPath(capture(resourceIdSlot)) } answers {
+            val resourceId = resourceIdSlot.captured
+            resourcePaths[resourceId]
+        }
+        coEvery { rootIndex.updateAll() } answers {
+            updatedShards += 1
+        }
         return rootIndex;
     }
 
@@ -77,12 +93,79 @@ class IndexAggregationTest {
         return stringBuilder.toString().toLong()
     }
 
+    private fun createResourceRandomPaths(resources: Set<Resource>): Map<ResourceId, Path> {
+        return resources.map { resource ->
+            val path = Path(UUID.randomUUID().toString())
+            resource.id to path
+        }.associateBy({ it.first }, { it.second })
+    }
+
     @Test
     fun testAllResources() = runBlocking {
         val expected = shards.map { it.allResources() }.toList().reduce { acc, map -> acc + map }
         val allResources = indexAggregation.allResources()
         assertEquals(9, allResources.size) // 3 resources per shard, so 3 shards * 3 resources
         assertEquals(expected, allResources)
+    }
+
+    @Test
+    fun testAllResourcesEmpty() = runBlocking {
+        val indexAggregation = IndexAggregation(setOf())
+        val allResources = indexAggregation.allResources()
+        assertEquals(0, allResources.size)
+        assertEquals(mapOf<ResourceId, Resource>(), allResources)
+    }
+
+    @Test
+    fun testGetResource() = runBlocking {
+        val shard = shards.random()
+        val resource = shard.allResources().values.random()
+        val result = indexAggregation.getResource(resource.id)
+        assertEquals(resource, result)
+    }
+
+    @Test
+    fun testGetResourceNotFound() = runBlocking {
+        val indexAggregation = IndexAggregation(setOf())
+        val result = indexAggregation.getResource(ResourceId(0, 123456789))
+        assertNull(result)
+    }
+
+    @Test
+    fun testGetAllPaths() = runBlocking {
+        val expected = shards.map { it.allPaths() }.toList().reduce { acc, map -> acc + map }
+        val result = indexAggregation.allPaths()
+        assertEquals(9, result.size) // 3 shards * 3 resource per shard
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testGetAllPathsEmpty() = runBlocking {
+        val indexAggregation = IndexAggregation(listOf())
+        val result = indexAggregation.allPaths()
+        assertEquals(0, result.size)
+        assertEquals(mapOf<ResourceId, Path>(), result)
+    }
+
+    @Test
+    fun testGetPath() = runBlocking {
+        val shard = shards.random()
+        val resource = shard.allResources().values.random()
+        val result = indexAggregation.getPath(resource.id)
+        assertTrue(result is Path)
+    }
+
+    @Test
+    fun getPathNotFound() = runBlocking {
+        val indexAggregation = IndexAggregation(setOf())
+        val result = indexAggregation.getResource(ResourceId(0, 123456789))
+        assertNull(result)
+    }
+
+    @Test
+    fun testUpdateAll() = runBlocking {
+        indexAggregation.updateAll()
+        assertEquals(3, updatedShards)
     }
 
 }
